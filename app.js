0684939dd35b9af366d53a66c7fe4931d1f4a4c5
Juan Paz <jcpazm@gmail.com>
2022-05-21 18:04:25 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Working in AWS now!'));
+app.get('/', (req, res) => res.send('Hello World! Working in AWS now! Testing the CodePipeline with Review Stage!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
